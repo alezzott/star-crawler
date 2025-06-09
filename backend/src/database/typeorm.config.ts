f6c67ccb3a6441618bb6caaaf2c78c546ba6066f
Alex Faustino <faustino.alex@outlook.com>
2025-06-09 10:36:24 -0300

    fix: db port

--- backend/src/database/typeorm.config.ts
+++ backend/src/database/typeorm.config.ts
@@ -4,7 +4,7 @@ import { Repository } from 'src/modules/github/repository.entity';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mariadb',
   host: process.env.DB_HOST || 'db',
-  port: parseInt(process.env.DB_PORT, 10) || 3306,
+  port: parseInt(process.env.DB_PORT || '3306', 10),
   username: process.env.DB_USER || 'user',
   password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'starcrawler',
